fd12501fec45fea55159d3b97405b226674ab23c
LALITHASHREE <dlalithashree@gmail.com>
2016-07-08 14:56:35 +0530

    finish

---
+++ README.c
@@ -0,0 +1,19 @@
+# prime-number
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the value of n");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the given number is prime number");
+}
+else
+{
+printf("the given  number is  not  a prime number");
+}
+return0;
+}
